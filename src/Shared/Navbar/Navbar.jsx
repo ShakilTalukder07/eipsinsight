76e79bed7829ba2de791046991034908d709d2fe
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-06-06 11:30:11 +0600

    logo add on navbar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import { Link } from "react-router-dom";
+import eipLogo from '../../assets/logo4.gif'
+import { BiUserCircle } from 'react-icons/bi'
 
 
 const Navbar = () => {
+
+    const menu = <>
+        <li>
+            <Link className="mx-4 text-xl hover:border-b-2 p-4 font-semibold border-b-white duration-100" to="/alleips">AllEIPs</Link>
+        </li>
+        <li>
+            <Link className="mx-4 text-xl hover:border-b-2 p-4 font-semibold border-b-white duration-100" to="/type">Type</Link>
+        </li>
+        <li>
+            <Link className="mx-4 text-xl hover:border-b-2 p-4 font-semibold border-b-white duration-100" to="/status">Status</Link>
+        </li>
+        <li>
+            <Link className="mx-4 text-xl hover:border-b-2 p-4 font-semibold border-b-white duration-100" to="/insight">Insight</Link>
+        </li>
+    </>
+
     return (
         <div>
             <div className="navbar bg-[#1B3838] text-white">
@@ -10,15 +28,14 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinknecap="round" strokeLinknejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-[#1B3838] w-[300px] mr-2 text-white rounded-box">
-                            <Link className="mx-3 mt-2 text-xl" to="/alleips">AllEIPs</Link>
-                            <Link className="mx-3 mt-2 text-xl" to="/type">Type</Link>
-                            <Link className="mx-3 mt-2 text-xl" to="/status">Status</Link>
-                            <Link className="mx-3 mt-2 text-xl" to="/insight">Insight</Link>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-[#1B3838] w-[350px] mr-2 text-white rounded-box">
+
+                            {menu}
+
                             <li tabIndex={0}>
                                 <details>
-                                    <summary className="text-xl">Resources</summary>
-                                    <ul className="p-2">
+                                    <summary className="text-xl hover:border-b-2 ml-4 p-4 font-semibold border-b-white duration-100">Resources</summary>
+                                    <ul className="p-6">
                                         <li><a>EIPs & Strandardization Process</a></li>
                                         <li><a>Ethereum Network Upgrade Process</a></li>
                                         <li><a>EIP:20 Token Standard</a></li>
@@ -27,18 +44,18 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </div>
-
-                    <Link to="/" className="normal-case text-3xl font-bold">EIPs</Link>
+                    <Link to="/" >
+                        <img className=" w-32 h-14 " src={eipLogo} alt="" />
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 bg-[#1B3838] text-white">
-                        <Link className="mx-4 mt-2 text-xl" to="/alleips">AllEIPs</Link>
-                        <Link className="mx-4 mt-2 text-xl" to="/type">Type</Link>
-                        <Link className="mx-4 mt-2 text-xl" to="/status">Status</Link>
-                        <Link className="mx-4 mt-2 text-xl" to="/insight">Insight</Link>
+
+                        {menu}
+
                         <li tabIndex={0}>
                             <details>
-                                <summary className="text-xl">Resources</summary>
+                                <summary className="text-xl hover:border-b-2 p-4 font-semibold border-b-white duration-100">Resources</summary>
                                 <ul className="p-2">
                                     <li className="bg-[#1B3838] rounded-lg my-1"><a>EIPs & Standardization Process</a></li>
                                     <li className="bg-[#1B3838] rounded-lg mb-1"><a>Ethereum Network Upgrade Process</a></li>
@@ -52,7 +69,7 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                               <BiUserCircle className="w-10 h-10"></BiUserCircle>
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
